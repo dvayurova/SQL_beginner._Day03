73f6cd290ddce678318361014d0547c7b8ffce31
Darina Ayurova <comedial@student.21-school.ru>
2023-04-07 21:30:38 +0300

    02

--- src/ex02/day03_ex02.sql
+++ src/ex02/day03_ex02.sql
@@ -1,4 +1,4 @@
-SELECT pizza_name, price, pizzeria.name 
+SELECT pizza_name, price, pizzeria.name AS pizzeria_name
 FROM menu
 INNER JOIN pizzeria ON pizzeria.id = menu.pizzeria_id
 WHERE menu.id NOT IN (SELECT menu_id FROM person_order)
